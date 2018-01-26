c3ea61847850ec84f5fd87c67e6e9bb158b4baa4
Derek Haller <dhaller.personal@gmail.com>
2018-01-26 04:39:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,7 @@ def sort_array_char_count(num_a)
 end
 
 def swap_elements(num_a)
-  array[1], array[2] = array[2], array[1]
+  num_a[1], num_a[2] = num_a[2], num_a[1]
 end
 def swap_elements_from_to(num_a, index, d_index)
   num_a.insert(d_index, num_a.delete_at(index))
