7151fc7fa36dbc37773481b7d12434ddfb9d3e4e
Derek Haller <dhaller.personal@gmail.com>
2018-01-26 04:52:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,9 +34,10 @@ def kesha_maker(str_a)
 end
 
 def find_a(str_a)
+  new_array = []
   str_a.find_all do |str|
     if str.start_with?("a")
-      puts str
+      new_array.push(str)
     end
   end
 end
